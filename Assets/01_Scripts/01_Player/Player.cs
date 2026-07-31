82b84c94c6118aed92aedef6bdc8be00147dd0fe
김형도 <bamtistudio@gmail.com>
2026-07-31 14:09:20 +0900

    타일맵, 시체 생성 코드 추가

--- Assets/01_Scripts/01_Player/Player.cs
+++ Assets/01_Scripts/01_Player/Player.cs
@@ -1,11 +1,40 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, InputSystem_Actions.IPlayerDieSelfActions
 {
+    #region 변수
     [SerializeField] private GameObject DeadPrefab;
     [SerializeField] private Transform resetPoint;
 
-    
+    private InputSystem_Actions inputActions;
+    #endregion
+
+    private void Awake()
+    {
+        
+        inputActions = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.PlayerDieSelf.SetCallbacks(this);
+        inputActions.PlayerDieSelf.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.PlayerDieSelf.Disable();
+    }
+
+    public void OnDie(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Die();
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("obstacle"))
@@ -13,6 +42,7 @@ public class Player : MonoBehaviour
             Die();
         }
     }
+
     [ContextMenu("죽음 테스트")]
     private void Die()
     {
