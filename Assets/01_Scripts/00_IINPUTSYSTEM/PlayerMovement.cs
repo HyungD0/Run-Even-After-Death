4cf776782341e0710bd34f399e9ccb3cd681d328
김형도 <bamtistudio@gmail.com>
2026-08-02 11:05:55 +0900

    태크 추가 및 버튼 초기 설정

--- Assets/01_Scripts/00_IINPUTSYSTEM/PlayerMovement.cs
+++ Assets/01_Scripts/00_IINPUTSYSTEM/PlayerMovement.cs
@@ -48,7 +48,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("corpse"))
         {
             isGround = true;
            
